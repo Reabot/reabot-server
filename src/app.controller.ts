f57336d6a6efb1fb66f110b0d579e2a39cfdf47c
Valentin Ichkour <valentin.ichkour@getmayday.co>
2020-10-09 13:27:24 +0200

    following documentation

---
+++ src/app.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+import { AuthService } from './auth/auth.service';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('/auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
